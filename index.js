2dc096a9389b7b6c56d6e98017bcc5f266872b68
denis <d.fedotovdenis@yandex.ru>
2025-05-14 01:21:41 +0300

    try fix mongo

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import express from "express";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 import router from "./router.js";
 // import cors from "cors";
 
 const PORT = 8182;
-// const DB_URL = "mongodb://localhost:27017/engraveDB";
+const DB_URL = process.env.MONGO_URI || "mongodb://localhost:27017/engraveDB";
 
 const app = express();
 
-// Разрешаем все CORS запросы
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "http://engraveart.space",
+    "https://engraveart.space",
+    "http://api.engraveart.space",
+    "https://api.engraveart.space",
+  ];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
@@ -30,14 +41,23 @@ app.get("/", (req, res) => {
   res.send("API сервер работает");
 });
 
+app.get("/test", (req, res) => {
+  res.json({
+    status: "success",
+    message: "Тестовый маршрут работает",
+    time: new Date().toISOString(),
+  });
+});
+
 async function startApp() {
   try {
-    // await mongoose.connect(DB_URL);
+    await mongoose.connect(DB_URL);
+    console.log("Подключение к MongoDB успешно установлено");
     app.listen(PORT, () => {
       console.log("Сервер завелся на порту", PORT);
     });
   } catch (error) {
-    console.log(error);
+    console.log("Ошибка подключения к MongoDB:", error);
   }
 }
 startApp();
